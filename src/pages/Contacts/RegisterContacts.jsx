e45a5e8984721dd1c4f8bdd9a6ec7513138c95a5
GFSprog40 <gustavo.prog40@gmail.com>
2023-07-24 13:35:11 -0300

    register conotacts done

--- src/pages/Contacts/RegisterContacts.jsx
+++ src/pages/Contacts/RegisterContacts.jsx
@@ -5,22 +5,22 @@ import api from '../../api'
 
 import { Link, useNavigate } from 'react-router-dom'
 import { Button } from '../../components/Buttons/styled-button'
+import HeaderComponent from '../../components/Header/Header'
 
 export const Container = styled.div`
   display: flex;
   width: 100vw;
-  height: 100vh;
+  height: auto;
   align-items: center;
-  justify-content: top;
+  justify-content: center;
   flex-direction: column;
-  /* background: orange; */
 `
 
 
 export const H1 = styled.h1`
   display: flex;
   align-items: center;
-  font-size: 50px;
+  font-size: 30px;
 
 
 
@@ -72,15 +72,16 @@ export const TextArea = styled.textarea`
   box-shadow: 0px 0px 5px 1px rgba(37, 0, 50, 0.25);
   font-family: 'Roboto';
   font-size: 1rem;
+  margin-left: 41px;
+  padding: 14px 14px;
 
   @media screen and (max-width: 850px) {
     display: flex;
-  /* width: 30%; */
   height: auto;
   align-items: center;
   justify-content: center;
     
-    width: 110%;
+    width: 100%;
   }
 `
 
@@ -96,25 +97,10 @@ function RegisterContacts() {
 
   const navigate = useNavigate()
 
-  // const token = localStorage.getItem('token')
-
-  // async function HandlePosts() {
-  //   const { data } = await api.get('/get-all-contacts')
-
-  //   setDados(data.data)
-
-  //   console.log(dados)
-
-  //   return <p></p>
-  // }
-
   async function handleSubmit(event) {
     event.preventDefault()
 
     try {
-      // console.log(`Token:${token}`)
-
-      // if (!token) return alert('Token Inválido, efetue o Login novamente!!')
 
       const data = {
         nome,
@@ -127,7 +113,7 @@ function RegisterContacts() {
 
       navigate('/')
 
-      return alert('Cadastro realizado com sucesso!')
+      return alert('Mensagem enviada com sucesso!')
     } catch (error) {
       return alert(error)
     }
@@ -155,50 +141,58 @@ function RegisterContacts() {
 
   return (
     <Container>
+      <HeaderComponent />
+
       <H1>ENTRE EM CONTATO CONOSCO</H1>
 
       <br />
       {/* {dados === 'OK' ? null : <h1>ACESSO PROIBIDO!!!</h1>} */}
       <br />
       <Form onSubmit={handleSubmit}>
-        <br />
+        <div style={{ marginTop: '-20px', width: '100%' }}>
 
-        <Input
-          placeholder="Nome"
-          invalid={true}
-          id="nome"
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
-        />
-        <br />
-        <Input
-          placeholder="Email"
-          invalid={true}
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br />
+          <Input
+            placeholder="Nome"
+            invalid={true}
+            id="nome"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+        </ div>
+        <div style={{ marginTop: '-40px', width: '100%' }}>
+          <Input
+            placeholder="Email"
+            invalid={true}
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </ div>
 
-        <Input
-          placeholder="Telefone"
-          invalid={true}
-          id="telefone"
-          value={telefone}
-          onChange={(e) => setTelefone(e.target.value)}
-        />
-        <br />
-        Mensagem:
-        <TextArea
-          rows="22"
-          cols="58"
-          id="message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
+        <div style={{ marginTop: '-40px', width: '100%' }}>
 
-        <br />
-        <Button type="submit">Enviar</Button>
+          <Input
+            placeholder="Telefone"
+            invalid={true}
+            id="telefone"
+            value={telefone}
+            onChange={(e) => setTelefone(e.target.value)}
+          />
+        </ div>
+
+        <div style={{ marginTop: '-22px', }}>
+          <TextArea
+            placeholder="Mensagem"
+            rows="12"
+            cols="42"
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+
+        </ div>
+
+        <Button style={{ marginTop: '20px', marginBottom: '70px' }} type="submit">Enviar</Button>
       </Form>
     </Container>
   )
