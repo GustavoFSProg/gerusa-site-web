acb55f72ceef04fd5d3eb3c0a3e7133a990eebfa
GFSprog40 <gustavo.prog40@gmail.com>
2023-07-25 10:33:40 -0300

    icone

--- src/style-app.jsx
+++ src/style-app.jsx
@@ -250,7 +250,7 @@ export const WhatsContainer = styled.div`
 
   @media screen and (max-width: 800px) {
    
-    top: 710px;
-            right: 24pt;
+    top: 410px;
+            right: 124pt;
   }
 `
