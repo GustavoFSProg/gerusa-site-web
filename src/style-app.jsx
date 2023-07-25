06f96db5d77c58af589773f39ec50bcc2bded5ed
GFSprog40 <gustavo.prog40@gmail.com>
2023-07-25 11:44:10 -0300

    fond

--- src/style-app.jsx
+++ src/style-app.jsx
@@ -216,12 +216,12 @@ export const WhatsContainer = styled.div`
 
 
   @media screen and (max-width: 360px) {
-    top: 500px;
+    top: 530px;
     right: 26pt;
   }
 
   @media screen and (min-width: 360px) and (max-width: 800px) {
     top: 616px;
-    right: 29pt;
+    right: 28pt;
   }
 `
