da9c0e6e7aa428123a010867c406dd0e7e04ef09
GFSprog40 <gustavo.prog40@gmail.com>
2023-07-25 10:51:03 -0300

    my whats

--- src/style-app.jsx
+++ src/style-app.jsx
@@ -250,7 +250,7 @@ export const WhatsContainer = styled.div`
 
   @media screen and (max-width: 800px) {
    
-    top: 420px;
-            right: 57pt;
+    top: 460px;
+            right: 45pt;
   }
 `
