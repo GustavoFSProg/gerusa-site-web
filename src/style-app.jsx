88b588069f560959e7baf86f1b4f5ceac6486809
GFSprog40 <gustavo.prog40@gmail.com>
2023-07-25 12:17:02 -0300

    comitando

--- src/style-app.jsx
+++ src/style-app.jsx
@@ -217,7 +217,7 @@ export const WhatsContainer = styled.div`
 
   @media screen and (max-width: 370px) {
     top: 576px;
-    right: 24pt;
+    right: 23pt;
   }
 
   @media screen and (min-width: 370px) and (max-width: 800px) {
