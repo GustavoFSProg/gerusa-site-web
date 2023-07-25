c2518b2c3f7c05c6dd4ec85d8bae645bf9aad139
GFSprog40 <gustavo.prog40@gmail.com>
2023-07-25 11:25:45 -0300

    my whatsasdfasdf3456asdfasdf

--- src/style-app.jsx
+++ src/style-app.jsx
@@ -217,7 +217,7 @@ export const WhatsContainer = styled.div`
   z-index: 100;
 
   @media screen and (max-width: 800px) {
-    top: 583px;
-    right: 45pt;
+    top: 585px;
+    right: 42pt;
   }
 `
