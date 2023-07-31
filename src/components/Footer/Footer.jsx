505df2e9a89f67344153ad50fba2d3447efcdefa
GFSprog40 <gustavo.prog40@gmail.com>
2023-07-31 17:48:40 -0300

    emailsss

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -15,7 +15,7 @@ export const LinkMobileEmail = styled.a`
  justify-content: center;
  z-index: 9999;
  width: 88%;
- font-weight: 457;
+ font-weight: 427;
  
 /* 
  @media screen and (max-width: 800px) {
