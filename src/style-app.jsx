b813039360fb708e7b8fba4323ba6f8dc9c0b493
GFSprog40 <gustavo.prog40@gmail.com>
2023-11-16 11:37:43 -0300

    news oiu

--- src/style-app.jsx
+++ src/style-app.jsx
@@ -113,7 +113,7 @@ export const ContainerText = styled.div`
   @media screen and (min-width: 300px) and (max-width: 450px) {
     flex-direction: column;
     justify-content: center;
-    margin-top: 140px;
+    margin-top: -20px;
     font-size: 17px;
     margin-bottom: 362px;
   }
