5a93fe7ff629342969e4589238c1e3dc2bcfb394
GusFSProg <gustavo.prog40@gmail.com>
2024-01-02 18:24:10 -0300

    pois

--- src/api.jsx
+++ src/api.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  // baseURL: 'http://localhost:5000'
-  baseURL: 'https://gerusa-site-api-juab.vercel.app'
+  baseURL: 'http://localhost:3250'
 })
 
 export const keys = {
