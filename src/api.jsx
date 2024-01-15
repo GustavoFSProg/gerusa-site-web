55ebf098ebc157e1e1d5c854d154f686dee088d4
GusFSProg <gustavo.prog40@gmail.com>
2024-01-15 11:15:25 -0300

    delete api url

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://gerusa-site-api-juab.vercel.app/',
+  baseURL: 'https://55555gerusa-site-api-juab.vercel.app/',
 })
 
 export const keys = {
